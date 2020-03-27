51fa1d010d7773d378b6af27dc734c9ae3d8fb5f
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2020-03-27 20:17:01 +0300

    Revert "Use eurlatgr as default console font"
    This reverts commit 42d93d348241106ef0c5af114a25c479f47bd8c1.
    https://github.com/dracutdevs/dracut/commit/42d93d348
    
    eurlatgr font does not support Cyrillic, Arabic etc. symbols
    We need them in ROSA.
    
    Signed-off-by: Mikhail Novosyolov <m.novosyolov@rosalinux.ru>

--- modules.d/10i18n/module-setup.sh
+++ modules.d/10i18n/module-setup.sh
@@ -26,7 +26,7 @@ install() {
     fi
 
     KBDSUBDIRS=(consolefonts consoletrans keymaps unimaps)
-    DEFAULT_FONT="${i18n_default_font:-eurlatgr}"
+    DEFAULT_FONT="${i18n_default_font:-LatArCyrHeb-16}"
     I18N_CONF="/etc/locale.conf"
     VCONFIG_CONF="/etc/vconsole.conf"
 
